f8a8feabcebdebae0a4b152d4919521dc5b4bd23
ADARSH KUMAR <adkumar9129@gmail.com>
2025-11-24 10:51:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request,gunicorn
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# --------------------------
+# Load Model
+# --------------------------
+model = pickle.load(open("LRModel.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # get input from HTML form
+    feature1 = float(request.form['feature1'])
+    feature2 = float(request.form['feature2'])
+    
+    # Convert to array
+    input_data = np.array([[feature1, feature2]])
+
+    # Predict
+    result = model.predict(input_data)[0]
+
+    return render_template('index.html', prediction=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
